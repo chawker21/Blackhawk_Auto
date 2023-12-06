73fc380cc5da07fcaf0422705ea1e25d9acac0c9
Chris Hawker <Madeye92@hotmail.com>
2023-12-06 16:52:53 -0700

    Dec2023- Landingpage

---
+++ public/GraphicalDocumentLanding.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LAMP Server Installation Guide</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            color: #333;
+            text-align: center;
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+            overflow: hidden;
+        }
+        header {
+            background: #50b3a2;
+            color: white;
+            padding-top: 30px;
+            min-height: 70px;
+            border-bottom: #e8491d 3px solid;
+        }
+        header a {
+            color: #ffffff;
+            text-decoration: none;
+            text-transform: uppercase;
+            font-size: 16px;
+        }
+        header ul {
+            padding: 0;
+            list-style: none;
+            text-align: center;
+        }
+        header li {
+            display: inline;
+            margin: 0 20px;
+        }
+        header #branding {
+            float: left;
+        }
+        header #branding h1 {
+            margin: 0;
+        }
+        header nav {
+            float: right;
+            margin-top: 10px;
+        }
+        header .highlight, header .current a {
+            color: #e8491d;
+            font-weight: bold;
+        }
+        header a:hover {
+            color: #ffffff;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <div class="container">
+        <div id="branding">
+            <h1><span class="highlight">Linux</span> LAMP Server Guide</h1>
+        </div>
+        <nav>
+            <ul>
+                <li class="current"><a href="http://35.164.36.90/GraphicalDocumentLanding.php">Home</a></li>
+                <li><a href="http://35.164.36.90/GraphicalDocument.php">Installation Guide</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<div class="container">
+    <h2>Welcome to the Easy-to-Follow LAMP Server Installation Guide</h2>
+    <p>This guide provides step-by-step instructions on how to install a LAMP (Linux, Apache, MySQL, PHP) server on your Linux system. It's designed for beginners and includes copy buttons for each command, making it easy to use and follow.</p>
+    <p><a href="http://35.164.36.90/GraphicalDocument.php">Click here to start the installation process!</a></p>
+</div>
+</body>
+</html>
+
